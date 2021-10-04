90992dd1d345547df0a9d0a3f264cd433ca98d31
Administrator <admin@example.com>
2021-10-04 23:45:22 +0200

    Update value

--- variables.tf
+++ variables.tf
@@ -1,14 +1,18 @@
 variable "files_path" {
+    type = string
     description = "Path of the input/output values"
     default = "."
 }
 variable "tkg_vault_ip" {
+    type = string
     description = "Vault server ip"
 }
 variable "cluster_name" {
+  type = string
   description = "Cluster Name"
 }
 variable "cluster_management_ip" {
+  type = string
   description = "Cluster Management IP"
 }
 
